5819ed21fd236979661ba0a4f1cf84e8cfb997d4
gustavotec <gustavotec138@gmail.com>
2023-08-12 22:40:19 -0300

    Alterar bebidas

--- Bibliotecas/__init__.py
+++ Bibliotecas/__init__.py
@@ -12,54 +12,57 @@ def menuFuncionario(senha, lista):
         print('**************************\n')
         print('1 = Modificar lista de clientes')
         print('2 = modificar lista de produtos')
-        opcao2 = int(input('Digite sua opcao: '))
-
-        match opcao2:
-            case 1:
-                print('1 = Alterar Dados Clientes')
-                print('2 = Excluir Clientes')
-                print('3 = Listar Clientes')
-                print('4 = Voltar ao Menu Inicial')
-                try:
-                    opcao3 = int(input('Digite sua opcao: '))
-                except ValueError:
-                    print('Digite um inteiro valido!!!')
-                else:
-                    match opcao3:
-                        case 1:
-                            clientes.alterarCliente()
-                        case 2:
-                            clientes.excluirCliente()
-                        case 3:
-                            clientes.listarCliente()
-                        case 4:
-                            exit(0)
-                        case _:
-                                print('Opcao Inexistente!!')
-            case 2:
-                print('1 = Adicionar Bebidas')
-                print('2 = Alterar Bebidas')
-                print('3 = Excluir Bebidas')
-                print('4 = Listar Bebidas')
-                print('5 = Voltar ao Menu Inicial')
-                try:
-                    opcao = int(input('Digite sua opcao: '))
-                except ValueError:
-                    print('Digite um inteiro valido!!!')
-                else:
-                    match opcao:
-                        case 1:
-                            bebidas.adicionarBebidas(lista)
-                        case 2:
-                            bebidas.alterarBebidas()
-                        case 3:
-                            bebidas.excluirBebidas()
-                        case 4:
-                            bebidas.listarBebidas(lista)
-                        case 5:
-                            exit(0)
-                        case _:
-                                print('Opcao Inexistente!!')
+        try:
+            opcao2 = int(input('Digite sua opcao: '))
+        except ValueError:
+            print('Digite um inteiro valido!!!')
+        else:
+            match opcao2:
+                case 1:
+                    print('1 = Alterar Dados Clientes')
+                    print('2 = Excluir Clientes')
+                    print('3 = Listar Clientes')
+                    print('4 = Voltar ao Menu Inicial')
+                    try:
+                        opcao3 = int(input('Digite sua opcao: '))
+                    except ValueError:
+                        print('Digite um inteiro valido!!!')
+                    else:
+                        match opcao3:
+                            case 1:
+                                clientes.alterarCliente()
+                            case 2:
+                                clientes.excluirCliente()
+                            case 3:
+                                clientes.listarCliente()
+                            case 4:
+                                exit(0)
+                            case _:
+                                    print('Opcao Inexistente!!')
+                case 2:
+                    print('1 = Adicionar Bebidas')
+                    print('2 = Alterar Bebidas')
+                    print('3 = Excluir Bebidas')
+                    print('4 = Listar Bebidas')
+                    print('5 = Voltar ao Menu Inicial')
+                    try:
+                        opcao = int(input('Digite sua opcao: '))
+                    except ValueError:
+                        print('Digite um inteiro valido!!!')
+                    else:
+                        match opcao:
+                            case 1:
+                                bebidas.adicionarBebidas(lista)
+                            case 2:
+                                bebidas.alterarBebidas()
+                            case 3:
+                                bebidas.excluirBebidas()
+                            case 4:
+                                bebidas.listarBebidas(lista)
+                            case 5:
+                                exit(0)
+                            case _:
+                                    print('Opcao Inexistente!!')
     else:
         print('Senha Invalida!!!')
 
--- Bibliotecas/bebidas.py
+++ Bibliotecas/bebidas.py
@@ -9,8 +9,60 @@ def adicionarBebidas(lista):
     lista.append(atual.copy())
     atual.clear()
 
-def alterarBebidas():
-    print('Digite o nome da Bebida:')
+def alterarBebidas(lista):
+    print('***********************')
+    print('*****ALTERAR BEBIDA*****')
+    print('**********************\n')
+    print('1 = Buscar por nome')
+    print('2 = Buscar por codigo')
+    try:
+        opcao = int(input('Digite a opcao desejada: '))
+    except ValueError:
+        print('Digite um inteiro valido!!!')
+    else:
+        if opcao == 1:
+            nome = input('Digite o nome da bebida que deseja altera: ')
+            for bebida in lista:
+                if nome == bebida['nome']:
+                    print('1 = Alterar nome')
+                    print('2 = Alterar quantidade')
+                    try:
+                        opcao2 = int(input('Digite a opcao desejada: '))
+                    except ValueError:
+                        print('Digite um inteiro valido!!!')
+                    else:
+                        match opcao2:
+                            case 1:
+                                bebida['nome'] = input('Digite o novo nome: ')
+                            case 2:
+                                try:
+                                    bebida['quantidade'] = int(input('Digite a nova quantidade: '))
+                                except ValueError:
+                                    print('Digite um inteiro valido!!!')
+        elif opcao == 2:
+            try:
+                codigo = int(input('Digite o codigo da bebida que deseja altera: '))
+            except ValueError:
+                print('Digite um inteiro valido!!!')
+            else:
+                for bebida in lista:
+                    if codigo == bebida['codigo']:
+                        print('1 = Alterar nome')
+                        print('2 = Alterar quantidade')
+                        try:
+                            opcao2 = int(input('Digite a opcao desejada: '))
+                        except ValueError:
+                            print('Digite um inteiro valido!!!')
+                        else:
+                            match opcao2:
+                                case 1:
+                                    bebida['nome'] = input('Digite o novo nome: ')
+                                case 2:
+                                    try:
+                                        bebida['quantidade'] = int(input('Digite a nova quantidade: '))
+                                    except ValueError:
+                                        print('Digite um inteiro valido!!!')
+
 
 def excluirBebidas():
     print('Digite o nome da Bebida:')
